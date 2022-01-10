5ea44ee25c7e74b2a1eaf2ef2f0f475efa9a68ea
ngoductrung <trung10032000@gmail.com>
2022-01-10 20:03:32 +0700

    New

--- main.py
+++ main.py
@@ -1,96 +1,107 @@
+#import libraries
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-# Import the dataset
-from sklearn.preprocessing._encoders import _BaseEncoder
+import seaborn as sns
 
-dataset = pd.read_csv('D:\Pycham\ATM\Atm_data_m2.csv')
-X = dataset.iloc[:, 1:10].values
-y = dataset.iloc[:, 10].values
+#loading the dataset
+df=pd.read_csv("D:\Pycham\ATM\Atm_data_m2.csv")
+df.head()
 
-# Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.compose import ColumnTransformer
-labelencoder_X_0 = LabelEncoder()
-X[:, 0] = labelencoder_X_0.fit_transform(X[:, 0])
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-labelencoder_X_2 = LabelEncoder()
-X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-labelencoder_X_3 = LabelEncoder()
-X[:, 3] = labelencoder_X_3.fit_transform(X[:, 3])
-labelencoder_X_4 = LabelEncoder()
-X[:, 4] = labelencoder_X_4.fit_transform(X[:, 4])
-labelencoder_X_5 = LabelEncoder()
-X[:, 5] = labelencoder_X_5.fit_transform(X[:, 5])
-# Encode variable with one hotencoder
+print(df.shape )
+#for knowing statastical values
+df.describe()
 
-onehotencoder = ColumnTransformer([(" ", OneHotEncoder() ,[1, 2, 3, 4, 5])], remainder = 'passthrough')
-X = onehotencoder.fit_transform(X).toarray()
+#check the datatype of each variable
+print(df.dtypes)
+print()
+#checking null values
+print(df.isnull().sum())
 
-# Splitting the dataset into the Training set and Test set
+#drop 1st and 2nd column from the dataset
+df = df.drop(df.columns[[0,1]], axis=1)
+
+categorcial_variables = ['trans_month','weekday']
+for col in categorcial_variables:
+    plt.figure(figsize=(10,4))
+    sns.barplot(df[col].value_counts().index,df[col].value_counts().values)
+    plt.title(col)
+    plt.tight_layout()
+
+#categorical to numerical
+df=pd.get_dummies(df,columns=['weekday','festival_religion','working_day','holiday_sequence'])
+df.shape
+
+#heatmap of correlation between each variable
+corr=df.corr()
+plt.figure(figsize=(20,20))
+sns.heatmap(df.corr(),annot=True)
+
+#selecting one feature amoung highly correlated varible
+columns = np.full((corr.shape[0],), True, dtype=bool)
+for i in range(corr.shape[0]):
+    for j in range(i+1, corr.shape[0]):
+        if corr.iloc[i,j] >= 0.9:
+            if columns[j]:
+                columns[j] = False
+selected_columns = df.columns[columns]
+df1 = df[selected_columns]
+
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+x = df1.drop(columns = ['total_amount_withdrawn'])
+y = df1['total_amount_withdrawn']
 
-# Feature Scaling
+
+xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=.20)
+print(xtrain.shape,xtest.shape,ytrain.shape,ytest.shape)
+
 from sklearn.preprocessing import StandardScaler
+sc= StandardScaler()
+xtrain = sc.fit_transform(xtrain)
+xtest = sc.transform(xtest)
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+#scaling target variable
+ytrain= sc.fit_transform(ytrain.values.reshape(-1,1))
+ytest= sc.transform(ytest.values.reshape(-1,1))
 
-# Part 2 - Now let's make the ANN!
-
-# Importing the Keras libraries and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
+
 # Initializing the ANN
 classifier = Sequential()
-
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(output_dim=25, init='uniform', activation='relu', input_dim=33))
-
+classifier.add(Dense(25, activation = 'relu', input_dim = 24))
 # Adding the 2 hidden layer
-classifier.add(Dense(output_dim=20, init='uniform', activation='relu'))
-
+classifier.add(Dense(20, activation = 'relu'))
 # Adding the 3 hidden layer
-classifier.add(Dense(output_dim=15, init='uniform', activation='relu'))
-
+classifier.add(Dense(15, activation = 'relu'))
 # Adding the 4 hidden layer
-classifier.add(Dense(output_dim=8, init='uniform', activation='relu'))
-
+classifier.add(Dense(8,  activation = 'relu'))
 # Adding the output layer
-classifier.add(Dense(output_dim=1, init='uniform', activation='linear'))
-
+classifier.add(Dense(1, activation = 'linear'))
 # Adding the output layer
-classifier.add(Dense(output_dim=1, init='uniform', activation='linear'))
-
+#classifier.add(Dense(1, activation = 'linear'))
 # Compiling the ANN
-classifier.compile(optimizer='adam', loss='mse')
-
+classifier.compile(optimizer = 'Adam', loss = 'mse')
 # Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size=10, nb_epoch=100)
-
+classifier.fit(xtrain, ytrain, batch_size=100,epochs = 500)
 # Part 3 - Making the predictions and evaluating the model
-
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
-
-# Print the predicted vs actual results
-for i in range(len(y_test)):
-    print("Y=%s, Predicted=%s" % (y_test[i], y_pred[i]))
-
-# Making the Confusion Matrix
-# from sklearn.metrics import confusion_matrix
+y_pred = classifier.predict(xtest)
 
 from sklearn import metrics
 
-print(metrics.mean_absolute_error(y_test, y_pred))
-print(metrics.mean_squared_error(y_test, y_pred))
-
-print(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-(np.sqrt(metrics.mean_squared_error(y_test, y_pred)) /
- np.mean(y_test))
+MAE_NN=metrics.mean_absolute_error(ytest, y_pred)
+MSE_NN=metrics.mean_squared_error(ytest, y_pred)
+RMSE_NN=np.sqrt(metrics.mean_squared_error(ytest, y_pred))
+r2_NN=metrics.r2_score(ytest,y_pred)
+print(MAE_NN)
+print(MSE_NN)
+print(RMSE_NN)
+print(r2_NN)
